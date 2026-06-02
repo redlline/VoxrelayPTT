2b6260267fd4e7fdc35abfc0687fd81a7125eef7
redlline <nixdatacomm@gmail.com>
2026-06-02 17:59:51 +0500

    chore: remove deploy scripts with credentials, scrub hardcoded password

--- deploy/init-pgbouncer.sh
+++ deploy/init-pgbouncer.sh
@@ -2,6 +2,6 @@
 set -e
 CONFIG=/etc/pgbouncer/pgbouncer.ini
 if ! grep -q 'password=' "$CONFIG"; then
-  sed -i 's|host=postgres port=5432 auth_user=voxrelay$|host=postgres port=5432 auth_user=voxrelay password=VoxRelay_Prod_DB_Pass_2024_Secure!|' "$CONFIG"
+  sed -i "s|host=postgres port=5432 auth_user=voxrelay$|host=postgres port=5432 auth_user=voxrelay password=${DB_PASSWORD}|" "$CONFIG"
 fi
 exec /entrypoint.sh /usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini
